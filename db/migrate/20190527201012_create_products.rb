6eacd04343da36d3b808204152c7aa18537e0a6f
Ricardo Ribeiro <ricardoc6r8@gmail.com>
2019-05-27 17:25:43 -0300

    Intend to generate a view for the available cars

---
+++ db/migrate/20190527201012_create_products.rb
@@ -0,0 +1,12 @@
+class CreateProducts < ActiveRecord::Migration[5.2]
+  def change
+    create_table :products do |t|
+      t.string :name
+      t.text :description
+      t.integer :price
+      t.string :category
+
+      t.timestamps
+    end
+  end
+end
