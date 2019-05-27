f0d4984f8aed3549e17767ba75d92a20508427df
Thiago Zanette <thiago_zs@yahoo.com>
2019-05-27 19:19:40 -0300

    model and migration added

---
+++ db/migrate/20190527220218_create_transactions.rb
@@ -0,0 +1,11 @@
+class CreateTransactions < ActiveRecord::Migration[5.2]
+  def change
+    create_table :transactions do |t|
+      t.integer :price
+      t.references :user, foreign_key: true
+      t.references :product, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
