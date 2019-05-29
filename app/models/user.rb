31957ee6b35133ef1545ae296ea5de1f1b60cf86
Ricardo Ribeiro <ricardoc6r8@gmail.com>
2019-05-29 11:09:58 -0300

    little fix sory for master push

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :products
-  has_many :orders
+  has_many :products, dependent: :destroy
+  has_many :orders, dependent: :destroy
 end
