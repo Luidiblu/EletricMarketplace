32912ab289e4c7cea2ee235667d66661ad6a042e
Lucas Souza <ibmstemp@gmail.com>
2019-05-31 16:20:21 -0300

    everything

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,6 +3,8 @@ class Product < ApplicationRecord
   validates :name, :description, :category, :price, presence: true
   has_many :orders
   mount_uploader :photo, PhotoUploader
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address?
 
   include PgSearch
   pg_search_scope :search_by_name_and_description,
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_30_170538) do
+ActiveRecord::Schema.define(version: 2019_05_31_184702) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,6 +35,9 @@ ActiveRecord::Schema.define(version: 2019_05_30_170538) do
     t.datetime "updated_at", null: false
     t.string "photo"
     t.boolean "sold"
+    t.float "latitude"
+    t.float "longitude"
+    t.text "address"
     t.index ["user_id"], name: "index_products_on_user_id"
   end
 
